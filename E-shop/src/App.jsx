04a55fd5758788c67bb65a06d88aabbe9d94f39d
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-07-16 15:12:22 +0200

    create folders and first files

--- E-shop/src/App.jsx
+++ E-shop/src/App.jsx
@@ -1,10 +1,15 @@
+import Navbar from '../components/composants/Navbar'
+import Home from '../components/pages/Home'
+import Footer from '../components/composants/Footer'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>Hellooooo</div>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
