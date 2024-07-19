e4c9d705527eade8951b91f432d2df7d7354d942
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-07-19 11:50:23 +0200

    ajout anim home

--- E-shop/src/App.jsx
+++ E-shop/src/App.jsx
@@ -1,8 +1,23 @@
+import React from "react";
 import Navbar from '../src/components/composants/Navbar'
 import Home from '../src/components/pages/Home'
 import Footer from '../src/components/composants/Footer'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+  React.useEffect(() => {
+    AOS.init(
+      {
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+        offset: 100,
+      }
+    );
+    AOS.refresh();
+  }, []);
 
   return (
     <div className="bg-brandWhite dark:bg-brandBlack dark:text-brandWhite
